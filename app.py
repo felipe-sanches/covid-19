b7d128b5ad6f395a7ad3b0a5a30c3357a42c5ce4
felipe-sanches <felipebsr97@gmail.com>
2020-09-07 15:02:05 -0300

    Versão 3

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#testando streamlit dploy
 
 # importar as bibliotecas necessárias
 import pandas as pd
@@ -8,73 +7,75 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import pydeck as pdk
 
-
-# importar os datasets COVID-19 para os respectivos DataaFrames
-cities = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities.csv')
+#Tratamento dos dados
+# importar os datasets COVID-19 para os respectivos DataFrames
 gps = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/gps_cities.csv')
-df_total = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-total.csv')
-
+covid_complete = pd.read_csv('https://raw.githubusercontent.com/wcota/covid19br/master/cases-brazil-cities-time.csv')
 
 #mudando o nome da coluna id para city
 gps = gps.rename(columns={'id': 'city'})
 
-#merge dos dataframes "cities" e "gps"
-df = cities.merge(gps)
-df.head()
+#merge dos dataframes "df_total" e "gps"
+df = covid_complete.merge(gps)
+
+#recorte de dados do ultimo dia
+lastday = max(df['date'])
+
+df_lastday = df.loc[df['date'] == lastday]
 
 
-
-#data dos dados
-st.write("Dados atualizados em {}".format(df['date'].max()))
-
+#informar data dos dados
+st.write("Dados atualizados até as 23:59h do dia {}".format(df['date'].max()))
 
 #título
-st.title("CENÁRIO DA COVID-19 NO BRASIL HOJE")
-
-#total de Casos
-st.info("Total de Casos: \n   {}".format(df['totalCases'].sum())) 
-
-#total de Mortes
-st.info("Total de Mortes: \n     {}".format(df['deaths'].sum()))
+st.title("CENÁRIO DA COVID-19 NO BRASIL")
 
 
-#descrição
+#descrição em texto
 st.markdown(
     """
-    Transmitido principalmente por meio de gotículas provenientes de tosses ou espirros de pessoas infectadas, a gravidade dos sintomas varia muito de pessoa para pessoa. 
-    Fato é, não se sabe muita coisa a respeito do COVID-19. 
-    
-    Estudos estão sendo realizados no mundo todo, porém os resultados ainda não são conclusivos e definitivos.
-    
-    Até o presente momento, observa-se que cerca de 80% dos casos confirmados são assintomáticos e rápidos. A maioria das pessoas que se encaixam nesse grupo, se recupera sem nenhuma sequela.
-    No entanto, 15% das pessoas terão infecções graves e precisarão de oxigênio. O restante das pessoas, que representam 5%, serão classificadas como infecções muito graves e precisarão de ventilação assistida, por meio de respiradores mecânicos em ambiente hospitalar.
-    Com o objetivo de elevar a consciência situacional a respeito do COVID-19 no Brasil, irei realizar uma análise sobre os dados públicos da doença.
+    Neste Dashboard Interativo é possível verificar o avanço do Corona Vírus no Brasil no decorrer do tempo.
+
+    Casos e óbitos confirmados por dia, utilizando informação oficial pelo Ministério da Saúde, dados no nível municipal do [Brasil.IO](https://brasil.io/) e dados mais recentes reportados pela equipe do [@CoronavirusBra1](https://twitter.com/CoronavirusBra1).
+    Acesso através do Repositório do Github do [Wesley Cota](https://github.com/wcota/covid19br).
+
+    Código-fonte disponível no meu [Portfólio no Github](https://github.com/felipe-sanches/data_science)
     """
 )
 
-#mapa
-st.subheader("Mapa Indicador")
+
+#slider semana epidemiológica
+semana = df['epi_week'].values.tolist()
+
+semana_epi = list(dict.fromkeys(semana))
+
+selected_week = st.slider("Escolha a semana epidemiológica", semana_epi[0], semana_epi[-1], value=semana_epi[-1])
+
+
+#slider data
+datas_semana_epi = df.loc[df['epi_week'] == selected_week]
+
+datas = datas_semana_epi['date'].values.tolist()
+
+dataslist = list(dict.fromkeys(datas))
+
+selected_date = st.selectbox("Escolha a data", dataslist)
+
+
+#recorte do dataframe para a data selecionada
+date_cut = df.loc[df['date'] == selected_date]
+
+
+#mapa indicador de cidades infectadas
+st.subheader("Mapa de Cidades Infectadas em {}".format(selected_date))
 st.text("")
-st.map(df)
-
-
-#sidebar
-
-
-# Add a slider to the sidebar:
-#qnt_cases = st.sidebar.slider('Nº de casos', 0, 2000000)
-
-
-
-
+st.map(date_cut)
 
 
 #grafico - cidades com maior numero de casos
-df.sort_values(['totalCases'], ascending=False)
+ten_cities = date_cut.sort_values(['totalCases'], ascending=False).head(10)
 
-ten_cities = df.sort_values(['totalCases'], ascending=False).head(10)
-
-fig, ax = plt.subplots(figsize=(15,8))
+fig, ax = plt.subplots(figsize=(15,10))
 
 ten_cities.plot(x='city', y='totalCases', kind="barh", ax=ax)
 
@@ -90,11 +91,9 @@ st.pyplot()
 
 
 #grafico cidades com maior numero de mortes
-df.sort_values(['deaths'], ascending=False)
+ten_deaths = date_cut.sort_values(['deaths'], ascending=False).head(10)
 
-ten_deaths = df.sort_values(['deaths'], ascending=False).head(10)
-
-fig, ax = plt.subplots(figsize=(15,7))
+fig, ax = plt.subplots(figsize=(15,10))
 
 ten_deaths.plot(x='city', y='deaths', kind="barh", ax=ax)
 
@@ -108,16 +107,18 @@ for index, value in enumerate(ten_deaths['deaths']):
 st.pyplot()
 
 
-#adicionando checkbox na sidebar
-selectbox = st.sidebar.selectbox("Exibir tabelas de dados", ('Escolha um dataframe','Cities', 'Gps', 'Total'))
-selected=None
+#sidebar
 
-if selectbox == 'Cities':
-    selected = cities
-elif selectbox == 'Gps':
-    selected = gps
-elif   selectbox == 'Total':
-    selected = df_total
+#total de Casos
+st.sidebar.warning("Total de Casos: {}".format(df_lastday['totalCases'].sum())) 
 
-st.write(selected)
-st.sidebar.info("Dataframe exibido no final da página")
+#total de Mortes
+st.sidebar.error("Total de Mortes: {}".format(df_lastday['deaths'].sum()))
+
+
+#adicionando selectbox na sidebar
+checkbox = st.sidebar.checkbox("Exibir tabelas de dados no final da página")
+
+if checkbox:
+    st.write(df)
+
